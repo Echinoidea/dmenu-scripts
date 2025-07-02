a8da7de352f68be7f35c0b1b713616da70114060
Gabriel Hooks <gabriel.i.hooks@gmail.com>
2025-07-02 12:01:31 -0400

    dmenu M-x remove .sh in display

--- dmenu-m-x.sh
+++ dmenu-m-x.sh
@@ -4,10 +4,10 @@
 
 SCRIPTS_DIR=~/dmenu-scripts
 
-SCRIPTS=$(find "$SCRIPTS_DIR" -maxdepth 1 -name "*.sh" -exec basename {} \;)
+SCRIPTS=$(find "$SCRIPTS_DIR" -maxdepth 1 -name "*.sh" -exec basename {} .sh \;)
 
 SELECTED_SCRIPT=$(echo "$SCRIPTS" | dmenu -l 10 -p "M-x:")
 
 if [ -n "$SELECTED_SCRIPT" ]; then
-    bash "$SCRIPTS_DIR/$SELECTED_SCRIPT"
+    bash "$SCRIPTS_DIR/$SELECTED_SCRIPT.sh"
 fi
