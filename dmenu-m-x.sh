c5fe8a8601f07c3ad462e2f8c48f50c828d9b184
Gabriel Hooks <gabriel.i.hooks@gmail.com>
2025-07-02 11:59:09 -0400

    dmenu M-x script launcher for these dmenu scripts

---
+++ dmenu-m-x.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Launch other dmenu scripts with dmenu, like Emacs M-x menu
+
+SCRIPTS_DIR=~/dmenu-scripts
+
+SCRIPTS=$(find "$SCRIPTS_DIR" -maxdepth 1 -name "*.sh" -exec basename {} \;)
+
+SELECTED_SCRIPT=$(echo "$SCRIPTS" | dmenu -l 10 -p "M-x:")
+
+if [ -n "$SELECTED_SCRIPT" ]; then
+    bash "$SCRIPTS_DIR/$SELECTED_SCRIPT"
+fi
