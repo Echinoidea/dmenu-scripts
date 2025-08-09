d81c17e7c0d3d1501927b366c4893dc3cde29134
Gabriel Hooks <gabriel.i.hooks@gmail.com>
2025-08-09 12:24:14 -0400

    bspwm spacious/compact padding

---
+++ compact-padding.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env sh
+
+bspc config top_padding 25
+bspc config left_padding 2
+bspc config right_padding 2
+bspc config bottom_padding 2
+bspc config window_gap 2
---
+++ spacious-padding.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env sh
+
+bspc config top_padding 36
+bspc config left_padding 16
+bspc config right_padding 16
+bspc config bottom_padding 16
+bspc config window_gap 8
